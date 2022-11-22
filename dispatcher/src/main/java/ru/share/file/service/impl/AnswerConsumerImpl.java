7a4e3a46968c4014572bb87ffdd8a502aeba3922
Buyniy <vtarasow163@gmail.com>
2022-11-22 16:48:48 +0400

    Добавлена NODE для получения сообщения из RabbitMQ, формирования ответа и отправки его в чат.

---
+++ dispatcher/src/main/java/ru/share/file/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package ru.share.file.service.impl;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.share.file.controller.UpdateController;
+import ru.share.file.service.AnswerConsumer;
+
+import static ru.share.file.RabbitQueue.*;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
---
+++ node/src/main/java/ru/share/file/service/impl/ProducerServiceImpl.java
@@ -0,0 +1,22 @@
+package ru.share.file.service.impl;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.share.file.service.ProducerService;
+
+import static ru.share.file.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class ProducerServiceImpl implements ProducerService {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProducerServiceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void producerAnswer(SendMessage sendMessage) {
+        rabbitTemplate.convertAndSend(ANSWER_MESSAGE, sendMessage);
+    }
+}
